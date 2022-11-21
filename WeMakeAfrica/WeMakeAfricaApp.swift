8bf3c65ba70f557d1c3c3d7b3b706b45b5f887e3
Ntwari Egide <ntwariegide2@gmail.com>
2022-11-21 19:39:22 +0200

    Initial Commit

---
+++ WeMakeAfrica/WeMakeAfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeMakeAfricaApp.swift
+//  WeMakeAfrica
+//
+//  Created by Mac on 21/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeMakeAfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
